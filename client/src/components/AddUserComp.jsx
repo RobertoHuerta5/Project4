079d7cb663ac53c4200b4840cb8c077722df7b55
Roberto Huerta <robertohuerta117@gmail.com>
2026-01-02 15:09:28 -0600

    update

--- client/src/components/AddUserComp.jsx
+++ client/src/components/AddUserComp.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AddUserComp() {
+    const [formData, setFormData] = useState ({
+        student_name: "", 
+        student_email: ""
+    })
+
+    function handleChange (event) {
+        const{ name,value } = event.target 
+        setFormData ({...formData, [name]:value})
+    }
   return (
     <div>AddUserComp</div>
   )
